abd35416b2369b5e62ae68dbc5ce5bb1f54731c5
prabha <prabha@gmail.com>
2021-07-06 07:00:26 +0530

    First commit

---
+++ src/main/java/com/amazon/App.java
@@ -0,0 +1,13 @@
+package com.amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Prabhakar, Hi Manish, Hi Pavan ----this is my first sample project!" );
+    }
+}
